27b47705010ad45b798ad99c54bfcf9fb9df4b8f
Javier <javierlogronoaguirre@gmail.com>
2023-09-22 17:38:47 +0200

    CHANGE LITERAL

--- src/app/pages/land/land-page-routing.module.ts
+++ src/app/pages/land/land-page-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
         path: '.',
         component: UndiagnosedPageComponent,
         data: {
-          title: 'dxGPT'
+          title: 'DxGPT'
         },
       },
       {
