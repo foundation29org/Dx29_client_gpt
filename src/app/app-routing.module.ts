f291393394f62f784cc7950af0e61e9d0291de3c
Javier <javierlogronoaguirre@gmail.com>
2024-08-01 13:10:43 +0200

    test routing change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,8 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy' })],
+  //imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 
