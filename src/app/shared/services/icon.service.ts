18a146a8949cbd1ca905abf39a41b637a6c859dc
Javier <javierlogronoaguirre@gmail.com>
2024-12-05 12:14:08 +0100

    test fontawesome

--- src/app/shared/services/icon.service.ts
+++ src/app/shared/services/icon.service.ts
@@ -10,16 +10,25 @@ export class IconsService {
     if (this.loaded) {
       return Promise.resolve();
     }
-
-    return new Promise((resolve) => {
+   /*return new Promise((resolve) => {
       const script = document.createElement('script');
-      script.src = 'https://kit.fontawesome.com/e1638781bd.js';
-      script.crossOrigin = 'anonymous';
+      script.src = 'assets/js/e1638781bd.js';
       script.onload = () => {
         this.loaded = true;
         resolve();
       };
       document.body.appendChild(script);
+    });*/
+
+    return new Promise((resolve) => {
+      // Cargar CSS principal
+      const link = document.createElement('link');
+      link.href = 'assets/fontawesome/free.min.css';
+      link.rel = 'stylesheet';
+      document.head.appendChild(link);
+
+      // No es necesario cargar el script JS
+      resolve();
     });
   }
 }
